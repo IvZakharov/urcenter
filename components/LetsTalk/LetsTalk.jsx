b1d7d1e113e01544a8652e83612c19c6817d4461
Ivan <iv@Ivans-MacBook-Air.local>
2022-12-05 21:19:59 +0300

    fix

--- components/LetsTalk/LetsTalk.jsx
+++ components/LetsTalk/LetsTalk.jsx
@@ -1,10 +1,17 @@
-import styles from './LetsTalk.module.scss';
-import React from 'react';
-import imageSvg from './img/img.svg';
-import { useMediaQuery, Container, Grid, Button, Box, TextField } from '@mui/material';
-import Image from 'next/image';
+import styles from "./LetsTalk.module.scss";
+import React from "react";
+import imageSvg from "./img/img.svg";
+import {
+  useMediaQuery,
+  Container,
+  Grid,
+  Button,
+  Box,
+  TextField,
+} from "@mui/material";
+import Image from "next/image";
 const LetsTalk = () => {
-  const matches = useMediaQuery('(min-width: 768px)');
+  const matches = useMediaQuery("(min-width: 768px)");
 
   return (
     <section className={styles.letsTalk} id="form">
@@ -18,10 +25,15 @@ const LetsTalk = () => {
           <Grid item xs={12} md={6} lg={7} order={matches ? 0 : 1}>
             <h2 className={styles.title}>ЗАКАЖИТЕ УСЛУГУ НА НАШЕМ САЙТЕ</h2>
             <p className={styles.text}>
-              Воспользуйтесь услугой нашего юридического центра сейчас, и вы сэкономите свое время,
-              деньги и нервы.
+              Воспользуйтесь услугой нашего юридического центра сейчас, и вы
+              сэкономите свое время, деньги и нервы.
             </p>
-            <Box component="form" noValidate autoComplete="off" className={styles.form}>
+            <Box
+              component="form"
+              noValidate
+              autoComplete="off"
+              className={styles.form}
+            >
               <Box className={styles.field}>
                 <label htmlFor="userName">Ваше имя:</label>
                 <input type="text" id="userName" name="userName" />
@@ -30,21 +42,40 @@ const LetsTalk = () => {
                 <label htmlFor="userTel">Телефон:</label>
                 <input type="tel" id="userTel" name="userTel" />
               </Box>
+            </Box>
+            <Box className={styles.buttons}>
+              <Button
+                sx={{
+                  width: matches ? "auto" : "100%",
+                  backgroundColor: "#fff",
+                  color: "primary.main",
+                  fontSize: 16,
+                  ":hover": {
+                    backgroundColor: "#e1e1e1",
+                  },
+                }}
+              >
+                Отправить заявку
+              </Button>
 
-              <Box className={styles.field}>
-                <Button
-                  sx={{
-                    width: matches ? 'auto' : '100%',
-                    backgroundColor: '#fff',
-                    color: 'primary.main',
-                    fontSize: 16,
-                    ':hover': {
-                      backgroundColor: '#e1e1e1',
-                    },
-                  }}>
-                  Отправить заявку
-                </Button>
-              </Box>
+              <Button
+                variant={"outlined"}
+                sx={{
+                  width: matches ? "auto" : "100%",
+                  backgroundColor: "transparent",
+                  color: "white",
+                  border: "1px solid white",
+                  whiteSpace: "nowrap",
+                  fontSize: 16,
+                  ":hover": {
+                    border: "1px solid white",
+                    backgroundColor: "white",
+                    color: "primary.main",
+                  },
+                }}
+              >
+                Написать в Whatsapp
+              </Button>
             </Box>
           </Grid>
         </Grid>
